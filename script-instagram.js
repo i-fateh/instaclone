aa71aedf5b21c57b1d9fcf038b9824124f81af67
i-fateh <fateh180581@gmail.com>
2022-11-17 09:46:09 +0100

    commit message

---
+++ script-instagram.js
@@ -0,0 +1,24 @@
+/* MESSAGE AUX RECRUTEURS :)
+    Ce code a été ecris il y a déja quelques années
+    Je le laisse dans l'état afin de garder une trace de mes progrès
+*/
+
+    function myFunction1() {document.getElementById("1").src = "./images-instagram/red-heart.png";}
+    function myFunction2() {document.getElementById("2").src = "./images-instagram/red-heart.png";}
+    function myFunction3() {document.getElementById("3").src = "./images-instagram/red-heart.png";}
+    function myFunction4() {document.getElementById("4").src = "./images-instagram/red-heart.png";}
+    function myFunction5() {document.getElementById("5").src = "./images-instagram/red-heart.png";}
+    function myFunction6() {document.getElementById("6").src = "./images-instagram/red-heart.png";}
+    function myFunction7() {document.getElementById("7").src = "./images-instagram/red-heart.png";}
+    function myFunction8() {document.getElementById("8").src = "./images-instagram/red-heart.png";}
+
+    function increment1() {document.getElementById("count_1").innerHTML = "130 J'aime";}
+    function increment2() {document.getElementById("count_2").innerHTML = "3573 J'aime";}
+    function increment3() {document.getElementById("count_3").innerHTML = "45 J'aime";}
+    function increment4() {document.getElementById("count_4").innerHTML = "232 J'aime";}
+    function increment5() {document.getElementById("count_5").innerHTML = "98 J'aime";}
+    function increment6() {document.getElementById("count_6").innerHTML = "766 J'aime";}
+    function increment7() {document.getElementById("count_7").innerHTML = "2432 J'aime";}
+    function increment8() {document.getElementById("count_8").innerHTML = "89 J'aime";}
+
+
